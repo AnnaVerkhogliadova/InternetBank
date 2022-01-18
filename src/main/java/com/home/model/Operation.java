2fa1a99a8584ff89bc267a24e82262172c5106e7
AnnaVerkhogliadova <verh2511@icloud.com>
2022-01-18 18:49:36 +0300

    add operations

---
+++ src/main/java/com/home/model/Operation.java
@@ -0,0 +1,54 @@
+package com.home.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "operations")
+public class Operation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "operation_id")
+    private int id;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "date", columnDefinition = "DATE")
+    private LocalDate date;
+
+    @Column(name = "type_of_operation")
+    private String TypeOfOperation;
+
+    @Column(name = "sum")
+    private int sum;
+
+    public Operation(int userId, LocalDate date, int sum, String typeOfOperation) {
+        this.userId = userId;
+        this.TypeOfOperation = typeOfOperation;
+        this.sum = sum;
+        this.date = date;
+    }
+
+    public Operation() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getTypeOfOperation() {
+        return TypeOfOperation;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+}
--- src/main/java/com/home/service/UserService.java
+++ src/main/java/com/home/service/UserService.java
@@ -1,18 +1,26 @@
 package com.home.service;
 
+import com.home.model.Operation;
 import com.home.model.User;
 
+import com.home.repository.OperationsRepository;
 import com.home.repository.UserRepository;
 import com.home.service.exception.BalanceException;
 import com.home.service.exception.UserNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
+@Transactional
 public class UserService {
     private final UserRepository userRepository;
+    OperationsRepository operationsRepository;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, OperationsRepository operationsRepository) {
         this.userRepository = userRepository;
+        this.operationsRepository = operationsRepository;
     }
 
     public User getBalance(int getBalance) {
@@ -21,9 +29,11 @@ public class UserService {
     }
 
     public User takeMoney(int id, int balance) {
+        LocalDate date = LocalDate.now();
         User user = getBalance(id);
         if (user.getId() == id) {
             if (user.getBalance() >= balance) {
+                operationsRepository.save(new Operation(id, date, balance,"снятие со счета"));
                 balance = user.getBalance() - balance;
                 user.setBalance(balance);
             } else {
@@ -37,7 +47,9 @@ public class UserService {
 
     public User putMoney(int id, int balance) {
         User user = getBalance(id);
+        LocalDate date = LocalDate.now();
         if (user.getId() == id) {
+            operationsRepository.save(new Operation(user.getId(), date, balance,"положить на счет"));
             balance = user.getBalance() + balance;
             user.setBalance(balance);
         } else {
